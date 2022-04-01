dd312f6342ed3ba8ece55b9127d7160147718a91
Tiago Vasconcelos <tisvasconcelos@gmail.com>
2022-04-01 19:04:41 -0300

    fix and adjust workflow

--- tools/dependency-map/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-// This is a workaround for https://github.com/eslint/eslint/issues/3458
-require('@rushstack/eslint-config/patch/modern-module-resolution');
-
-module.exports = {
-  plugins: ['@loft'],
-  extends: ['plugin:@loft/recommended-backend'],
-};
--- tools/dependency-map/README.md
+++
@@ -1,4 +0,0 @@
-# dependency-map
-
-This small project enables us to create scripts that automates finding unused files for examples.
-It can create a dependency graph across entire project using madge and with that graph we can do any kinds of things
